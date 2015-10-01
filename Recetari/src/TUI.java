7a176c69dfd6cce6e92217fbe5337a97afe7388f
FrodOctopuss <mrdelayman@gmail.com>
2015-10-01 18:44:31 +0200

    In Procass

--- Recetari/src/TUI.java
+++ Recetari/src/TUI.java
@@ -1,10 +1,21 @@
+import java.util.LinkedList;
 
 public class TUI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Allergen Gluten = new Allergen( 1,"Gluten");
-			Allergen Melc = new Allergen( 2,"Melc");
+		Allergen glutenAllergen = new Allergen( 1,"Gluten");
+		Allergen dairyAllergen = new Allergen( 2,"Dairy");
+		Allergen eggsAllergen = new Allergen( 3,"Eggs");
+		
+		LinkedList<Allergen> allergens = new LinkedList<Allergen>();
+		allergens.offer(glutenAllergen);
+		allergens.offer(dairyAllergen);
+		Ingredient milk = new Ingredient(1, "Milk", "Units", 25.4, 50, 2, 0, 172, allergens);
+		allergens.clear();
+		allergens.offer(eggsAllergen);
+		Ingredient eggs = new Ingredient(2, "Eggs", "Units", 25.4, 50, 2, 0, 172, allergens);
+		allergens.clear();
 
 	}
 
