1a81434b8adfcb21679185e7fb6b5a2379a9306d
FrodOctopuss <mrdelayman@gmail.com>
2015-11-24 18:44:17 +0100

    CookingProcedure beta

--- Recetari/src/CookingProcedure.java
+++ Recetari/src/CookingProcedure.java
@@ -14,8 +14,37 @@
 
 
 public class CookingProcedure {
-	int code;
-	String name;
+	HashMap<Integer, String> procedures = new HashMap<Integer, String>();
+
 	
-	//CookingProcedure(){}
+	public CookingProcedure(){
+		procedures.put(1,"Bake in oven");
+		procedures.put(2,"Boil");
+		procedures.put(3,"Fry");
+		procedures.put(4,"Whip"); //Montar
+		procedures.put(5,"Heat up");
+		procedures.put(6,"Heat up in microwave oven");
+		procedures.put(7,"Blend"); //Batir
+		procedures.put(8,"Mix");
+		procedures.put(9,"Fry lightly");
+	}
+
+	/* proceedIngredient method
+	 * 
+	 * @param procedureCode
+	 * @param ingredientCode
+	 * @param quantity
+	 * @param times
+	 * @param time
+	 * @param temperature
+	 * 
+	 * @return a String with the hole procedure
+	 * */
+	public String proceedIngredient(int procedureCode, int ingredientCode, int quantity, int times, int time, int temperature){
+		String stepText = procedures.get(procedureCode) + quantity;
+		return stepText;
+	}
+	public String simpleStep(String stepText) {
+		return stepText;
+	}
 }
