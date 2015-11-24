5481a4bc0376eac3e43d8b71f15e90eec0d70798
kizors <perico@hotmail.com>
2015-11-24 18:42:46 +0100

    Ingredient,Recipe i TUI Actualitzat

--- Recetari/src/Recipe.java
+++ Recetari/src/Recipe.java
@@ -18,32 +18,38 @@ public class Recipe {
 	
 	private int code;
 	private String name;
-	private ArrayList<Ingredient> ingredientList = new ArrayList<Ingredient>();
-	private ArrayList<Integer> quantity = new ArrayList<Integer>();
+	private HashMap<Ingredient, Integer> ListIngredients = new HashMap();
 	private double kCal;
 	private double carbohydrates;
 	private double proteins;
 	private double fat;
 	private double salt;
-	private ArrayList<Allergen> allergens;
+	private HashSet<Allergen> allergens;
 	
 	public Recipe(int code, String name, ArrayList<Ingredient> ingredientList, ArrayList<Integer> quantity, double kCal, double carbohydrates, double proteins, double fat, 
 			double salt) {
 		this.code = code;
 		this.name = name;
-		this.ingredientList = ingredientList;
 		this.quantity = quantity;
 		this.kCal = kCal;
 		this.carbohydrates = carbohydrates;
 		this.proteins = proteins;
 		this.fat = fat;
 		this.salt = salt;
-		this.allergens = allergens;
+		for ( int i : quantity){
+			int count = 0;
+			ListIngredients.put(ingredientList.get(count), quantity.get(i));
+			count++;
+		}
+		for ( Ingredient ing : ListIngredients){
+			allergens.add(ing.getAllergens());
+		}
+		
 	}
 	
-	public void getAllergenList(){
+	/*public void getAllergenList(){
 		for ( Ingredient ing : this.ingredientList ) {
 			
 		}
-	}
+	}*/
 }
